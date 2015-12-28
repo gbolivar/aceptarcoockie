2e4b03cbffc5491d3f107ecabf1d022d0dfe8092
Gregorio Bolivar <elalconxvii@gmail.com>
2015-12-28 11:51:17 -0430

    Subiendo el codigo para lo del coockie ejemplo implementado

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="utf-8">
+<title>Aceptación de Cookie</title>
+<link href="default.css" rel="stylesheet" type="text/css" />
+
+<!-- cookies -->
+<script type="text/javascript">
+    window.cookieconsent_options = {"message":"Este sitio utiliza cookies para una mejor experiencia, si sigues adelante aceptas la mismas.","dismiss":"Adelante","learnMore":"Más info.","link":"http://colorvivo.com/privacidad-cookies/","theme":"dark-bottom"};
+</script>
+
+<script type="text/javascript" src="cookieconsent.latest.min.js"></script>
+</head>
+<body>
+	<div id="header">
+		
+			<h1 id="blogname">Código GNU General Public License (GPLv3)</h1>
+            Te permite aceptar el coockie del Navegador.
+		
+	</div>
+	<div id="content">
+    </div>
+	<div id="footer">
+		<p class="txtCenter txtSmall">Este software es <a href="http://www.recursosgratis.com" title="gratis">gratis</a> y está disponible para distribuir bajo licencia <a href="http://www.gnu.org/licenses/gpl-3.0.txt" target="_blank">GNU General Public License (GPLv3)</a>.</p>
+	</div>
+
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100636791ns.gif" /></p></noscript>
+        
+</body>
+</html>
